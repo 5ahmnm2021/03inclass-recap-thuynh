2e91676fd010e6f18a98718a53f5cb17fbaad0a2
orvilline <thunhi.h@gmail.com>
2021-03-09 08:24:24 +0100

    added Scene Switch

---
+++ Assets/MyGame/scripts/controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("01ColorScene");
+    }
+    public void ChangeScenetoNum()
+    {
+        SceneManager.LoadScene("02NumberScene");
+
+    }
+    public void ChangeScenetoWelcome()
+    {
+        SceneManager.LoadScene("00WelcomeScene");
+
+    }
+}
