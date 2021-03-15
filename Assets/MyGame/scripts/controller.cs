9fe87a01049dbe80856777dc161926779586dee9
orvilline <thunhi.h@gmail.com>
2021-03-15 17:42:09 +0100

    try and catch added

--- Assets/MyGame/scripts/controller.cs
+++ Assets/MyGame/scripts/controller.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class controller : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,4 +33,49 @@ public class controller : MonoBehaviour
         SceneManager.LoadScene("00WelcomeScene");
 
     }
+
+    public InputField A;
+    public InputField B;
+    public int Result;
+    public Text TxtResult;
+    public Text Error;
+    public Color Red = Color.red;
+    public Color White = Color.white;
+    public float InputAFl;
+    public float InputBFl;
+
+    public void AddNumbers()
+    {
+        try
+        {
+            InputAFl = float.Parse(A.text);
+            A.image.color = White;
+        }
+
+        catch(System.Exception)
+        {
+            A.image.color = Red;
+            Error.text = "Please enter a number";
+        }
+
+        try
+        {
+            InputBFl = float.Parse(B.text);
+            B.image.color = White;
+        }
+
+        catch (System.Exception)
+        {
+            B.image.color = Red;
+            Error.text = "Please enter a number";
+        }
+
+        Result = int.Parse(A.text) + int.Parse(B.text);
+        Debug.Log(Result);
+        TxtResult.text = Result.ToString();
+
+    }
+
+
+
 }
