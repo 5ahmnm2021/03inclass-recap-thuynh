85e9cbc96bd0b7afc6048a1a01af6e878ca377af
orvilline <thunhi.h@gmail.com>
2021-03-16 09:56:05 +0100

    try and catch fixed

--- Assets/MyGame/scripts/controller.cs
+++ Assets/MyGame/scripts/controller.cs
@@ -34,45 +34,63 @@ public class controller : MonoBehaviour
 
     }
 
-    public InputField A;
-    public InputField B;
-    public int Result;
-    public Text TxtResult;
-    public Text Error;
-    public Color Red = Color.red;
-    public Color White = Color.white;
-    public float InputAFl;
-    public float InputBFl;
+    public InputField a;
+    public InputField b;
+    public int result;
+    public Text txtResult;
+    public Text error;
+    public Color red = Color.red;
+    public Color white = Color.white;
+    //private int inputAInt;
+    //private int inputBInt;
+    public bool num1 = true;
+    public bool num2 = true;
 
     public void AddNumbers()
     {
+       int inputAInt = 0;
+       int inputBInt = 0;
+
         try
         {
-            InputAFl = float.Parse(A.text);
-            A.image.color = White;
+            inputAInt = int.Parse(a.text);
+            num1 = true;
+            a.image.color = white;
         }
 
         catch(System.Exception)
         {
-            A.image.color = Red;
-            Error.text = "Please enter a number";
+            num1 = false;
+            a.image.color = red;
+            error.text = "Please enter a number";
         }
 
         try
         {
-            InputBFl = float.Parse(B.text);
-            B.image.color = White;
+            inputBInt = int.Parse(b.text);
+            num2 = true;
+            b.image.color = white;
         }
 
         catch (System.Exception)
         {
-            B.image.color = Red;
-            Error.text = "Please enter a number";
+            num2 = false;
+            b.image.color = red;
+            error.text = "Please enter a number";
         }
 
-        Result = int.Parse(A.text) + int.Parse(B.text);
-        Debug.Log(Result);
-        TxtResult.text = Result.ToString();
+        Debug.Log("a" + inputAInt + " b" + inputBInt);
+
+        Debug.Log(num1+" "+num2);
+        if (num1 && num2)
+        {
+            result = inputAInt + inputBInt;
+            Debug.Log(result);
+            txtResult.text = result.ToString();
+        }
+        
+        //Result = int.Parse(a.text) + int.Parse(B.text);
+        
 
     }
 
